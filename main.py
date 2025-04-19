1c608f89ac837702742adc80b771c2837c8f21aa
Eretty <127791537+Eretty@users.noreply.github.com>
2025-04-19 20:42:50 +0200

    replace gui.py to main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox, ttk
 
-from sender import senderModelKeldaria
+from sender.sender import senderModelKeldaria
 
 
 class KeldariaTool:
